e28f966f5873ebe7458a37a4037d8ee060157f37
gregjazzy <gregjazzy@gmail.com>
2025-08-31 19:11:53 +0200

    fix: supprime les imports inutilisés de MathJax, Chart et Lottie

--- lib/lazyLibraries.ts
+++ lib/lazyLibraries.ts
@@ -17,24 +17,6 @@ export const LazyAnimatePresence = dynamic(() => import('framer-motion').then(mo
   ssr: false
 });
 
-// Math components (à adapter selon vos besoins)
-export const LazyMathJax = dynamic(() => import('react-mathjax'), {
-  ssr: false,
-  loading: () => <div>Chargement des formules...</div>
-});
-
-// Chart components (à adapter selon vos besoins)
-export const LazyChart = dynamic(() => import('react-chartjs-2').then(mod => mod.Line), {
-  ssr: false,
-  loading: () => <div>Chargement du graphique...</div>
-});
-
-// Animation components
-export const LazyLottie = dynamic(() => import('lottie-react'), {
-  ssr: false,
-  loading: () => <div>Chargement de l'animation...</div>
-});
-
 // Utility function to create lazy loaded components
 export function createLazyComponent<T>(importFn: () => Promise<T>) {
   return dynamic(() => importFn(), {
